d9a9a306853a7916e3c22d06fd8891b5edaa2ad1
ZacheusOrzechowski1998 <137503839+ZacheusOrzechowski1998@users.noreply.github.com>
2023-06-29 03:51:11 +0700

    Add support for different file formats

---
+++ cat_lion_nature.r
@@ -0,0 +1,37 @@
+# The Career Exploration Project
+
+# Defining overall objectives
+objective <- "To gain an understanding of the various career paths available to individuals seeking to begin a new career or transition to a new career"
+
+# Establishing knowledge base
+research <- c("Research available careers", "Interview individuals in relevant career paths", "Gather information on conventional and unconventional career paths", 
+              "Analyze the data and trends from available sources", "Gather feedback from relevant experts", "Connect with alumni to explore career paths")
+
+# Setting Goals and Milestones
+# Research
+init_research <- c("Create list of 10-20 career paths to research","Conduct initial research on careers to gain understanding")
+
+# Interviews
+interview_goals <- c("Create list of individuals to contact and interview","Schedule and conduct interviews")
+
+# Gather Data
+data_collection <- c("Compile data from available sources","Create summary report of data collected")
+
+# Analysis
+analysis_goals <- c("Conduct analysis on information gathered", "Produce visualizations to illustrate findings")
+
+# Evaluate feedback
+experts_opinion <- c("Gather feedback from relevant experts","Propose and test hypotheses based on feedback")
+
+# Alumni Connections
+alumni_connection <- c("Identify relevant alumni to connect with","Develop and strengthen relationships")
+
+# Reporting Outcome
+final_report <- c("Write a final report summarizing research and findings","Develop a list of recommendations")
+
+# Setting timeline
+timeline <- c("Create timeline for project goals and milestones, 
+               estimated completion date for project should be 3 months from start date")
+
+# Allocation of Resources
+resources <- c("Gather resources needed to complete project","Assign/Allocate tasks to relevant team members")
